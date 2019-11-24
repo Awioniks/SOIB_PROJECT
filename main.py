6920001d548486ac514fcc507b10a9f5bf25a2c0
Awioniks <patricosmodzel@gmail.com>
2019-11-24 17:37:09 +0100

    NEW HOT SHIT

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+# main func making Commutation Field building
+
+from argparse import ArgumentParser
+import sys
+import commutation.commutation_field as com
+import commutation.commutator_globals as glob
+
+
+def main(file_to_read):
+    """
+    main func which build commutation field
+    """
+    consts = glob.Const_For_Commutators()
+    consts.aggregate_consts(file_to_read)
+    commutation_field = com.Commutation_Field()
+    key = "{}_{}"
+
+    for section_nr, connection in consts.CONNECTIONS.items():
+        for nr_in_section, addresses in connection["commutator"].items():
+            identity = key.format(section_nr, nr_in_section)
+            commutation_field.add_commutator(section_nr, nr_in_section)
+            commutation_field.set_addresses_of_commutator(identity, addresses)
+
+    commutation_field.show_com()
+
+
+if __name__ == "__main__":
+    file_to_read = sys.argv[1]
+    main(file_to_read)
